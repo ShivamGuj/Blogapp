4371f9badac48f12f6a7fdff4ad0aed75e7be373
ShivamGuj <shivamgujaria4@gmail.com>
2024-08-25 13:45:33 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'blog_app',
-    password: 'shivam14',
-    port: 5432,
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
 });
 
 module.exports = pool;
